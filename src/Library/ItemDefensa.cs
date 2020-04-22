1dff4140f1032c28c8a53bc2b73b64b67a5af2fe
asanchez412 <sanchezandres41997@gmail.com>
2020-04-21 21:42:42 -0300

    Agrega ItemDefensa

---
+++ src/Library/ItemDefensa.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace ItemDefensa
+{
+    class ItemDefensa
+    {
+        public ItemDefensa(string name, string description, int armorValue)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.ArmorValue = armorValue;
+        }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int ArmorValue 
+        {
+            get {return this.ArmorValue;}
+            set
+            {
+                if (value < 0)
+                {
+                    this.ArmorValue = 0;
+                }
+                else
+                {
+                    this.ArmorValue = value;
+                }
+            }
+        }
+    }
+}
