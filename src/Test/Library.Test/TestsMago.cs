438624de3845ae363325df3c4c90ce5bb82aa8d9
ValentinaSilveira <62141464+ValentinaSilveira@users.noreply.github.com>
2020-04-23 02:34:47 -0300

    Agrego Tests de ataque desde Mago

--- src/Test/Library.Test/TestsMago.cs
+++ src/Test/Library.Test/TestsMago.cs
@@ -5,16 +5,16 @@ using Elfos;
 using ItemsDeAtaque;
 using ItemsDeDefensa;
 
-namespace TestsEnano
+namespace TestsMago
 {
     [TestFixture]
-    public class TestsDeEnano
+    public class TestsDeMago
     {
         [SetUp]
         public void SetUp()
         {
             Enano p1 = new Enano("Jorginho", 100, 10, 5);
-            Enano p2 = new Enano("Pepinho", 100, 8, 6);
+            Mago p2 = new Mago("Georghino", 100, 7, 4);
             Mago p3 = new Mago("Dainamo", 100, 6, 3);
             Elfo p4 = new Elfo("Pablinho", 100, 7, 5);
             ItemAtaque off1 = new ItemAtaque("Hacha","Hacha de dos mertros", 8);
@@ -28,6 +28,7 @@ namespace TestsEnano
             ItemDefensa deff6 = new ItemDefensa("Coraza de metal de alta calidad","La mejor coraza", 7);
             ItemDefensa deff7 = new ItemDefensa("Pantalones de cuero","Ligeros", 3);
 
+            
             p1.EquipOffEquip(off1);
             p1.EquipOffEquip(off3);
             p1.EquipDeffEquip(deff1);
@@ -35,12 +36,17 @@ namespace TestsEnano
             p1.EquipDeffEquip(deff3);
             p1.EquipDeffEquip(deff7);
 
-            p2.EquipOffEquip(off2);
-            p2.EquipDeffEquip(deff5);
-
             LibrodeHechizos libro1 = new LibrodeHechizos("Rayos", 8);
+            
+            p2.AddLibro(libro1);
+            p2.EquipOffEquip(off2);
+            p2.EquipDeffEquip(deff1);
+            p2.EquipDeffEquip(deff3);
+            p2.EquipDeffEquuip(deff7);
+
             p3.AddLibro(libro1);
             p3.EquipOffEquip(off3);
+            p2.EquipDeffEquip(deff3);
             p3.EquipDeffEquip(deff5);
             p3.EquipDeffEquip(deff1);
 
@@ -52,25 +58,25 @@ namespace TestsEnano
 
         public void IsGetAttackValueRight()
         {
-            Assert.AreEqual(p1.GetAttackValue(), 23);
+            Assert.AreEqual(p2.GetAttackValue(), 22);
         }
         [TestCase]
         public void IsGetDeffValueRight()
         {
-            Assert.AreEqual(p1.GetDeffValue(), 21);
+            Assert.AreEqual(p2.GetDeffValue(), 8);
         }
         [TestCase]
         public void IsAtacarEnanoRight()
         {
-            Assert.AreEqual(p1.AtacarEnano(p2), $"El jugador {p2.Name} recibe 14 puntos de daño");
+            Assert.AreEqual(p2.AtacarEnano(p1), $"El jugador {p1.Name} recibe 14 puntos de daño");
         }
         public void IsAtacarMagoRight()
         {
-            Assert.AreEqual(p1.AtacarMago(p3), $"El jugador {p3.Name} recibe 12 puntos de daño");
+            Assert.AreEqual(p2.AtacarMago(p3), $"El jugador {p3.Name} recibe 12 puntos de daño");
         }
         public void IsAtacarElfoRight()
         {
-            Assert.AreEqual(p1.AtacarElfo(p4), $"El jugador {p4.Nickname} recibe 8 puntos de daño");
+            Assert.AreEqual(p2.AtacarElfo(p4), $"El jugador {p4.Nickname} recibe 8 puntos de daño");
         }
     }
 }
